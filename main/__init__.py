7e73ae5a721ade79939ec69c7bc36f5101dec248
김재민 <kimjaemin@gimjaemin-ui-MacBookAir.local>
2023-11-15 19:57:13 +0900

    Remove Markdown function

--- main/__init__.py
+++ main/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from flaskext.markdown import Markdown
 
 import config
 
@@ -38,8 +37,5 @@ def create_app():
     # 필터
     from .filter import format_datetime
     app.jinja_env.filters['datetime'] = format_datetime
-
-    # markdown
-    Markdown(app, extensions=["nl2br", "fenced_code"])
 
     return app
